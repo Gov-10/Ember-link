f398634c7cac128db10dd9a27454b17b6d861276
Govind S <govindsys1008@gmail.com>
2026-03-31 23:31:31 +0530

    EmberUser model created

--- backend/backapp/api.py
+++ backend/backapp/api.py
@@ -1,7 +1,12 @@
 from ninja import NinjaAPI
+import os
 
 api = NinjaAPI()
 
 @api.get("/health")
 def healthchek(request):
     return {"status": "RUNNING"}
+
+
+
+
--- backend/backapp/models.py
+++ backend/backapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+class EmberUser(models.Model):
+    ROLES= (("user", "User"), ("ngo", "NGO"))
+    cognito_sub=models.CharField(max_length=500, unique=True)
+    role=models.CharField(max_length=20, choices=ROLES)
+    phone=PhoneNumberField(region='IN', blank=True)
+    location=models.CharField(null=True, blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.phone}->{self.role}"
+
