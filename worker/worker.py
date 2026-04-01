f3e9a8e3dc1be28622cee6c45bf305d86e9d3273
Govind S <govindsys1008@gmail.com>
2026-04-01 21:06:44 +0530

    Worker updated

--- worker/worker.py
+++ worker/worker.py
@@ -1,1 +1,28 @@
-#TODO: Subscriber code written here
+#TODO: pubsub subscriber code add karna-> pending
+import redis
+import json, os
+from dotenv import load_dotenv
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+load_dotenv()
+
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT")),
+    password=os.getenv("REDIS_PASSWORD"),
+    decode_responses=True
+)
+
+def publish_update(region, data):
+    redis_client.set(
+        f"flood:latest:{region}",
+        json.dumps(data),
+        ex=300
+    )
+    async_to_sync(channel_layer.group_send)(
+        f"flood_{region}",
+        {
+            "type": "send_update",
+            "data": data
+        }
+    )
